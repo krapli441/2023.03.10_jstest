b2aab35ab59f4fc2e2f9bcb66df559799148f888
박준형 <108733795+krapli441@users.noreply.github.com>
2023-03-16 12:25:03 +0900

    entries 실험

--- source/clickevent.js
+++ source/clickevent.js
@@ -34,21 +34,24 @@ teamMateListForBtn[0].addEventListener("click", switchToggle);
 
 function switchToggle () {
   if (changeSwitch === true) {
-  for (let i = 0; i < teamMateListForBtn.length; i++) {
-    const changeIcon = teamMateListForBtn[i].children;
-    for (let j = 0; j < changeIcon.length; j++) {
-      changeIcon[j].style.display = "none";
-    }
     changeSwitch = false;
-      if (changeSwitch === false) {
-        teamMateListForBtn[i].addEventListener("click", function()
-        {
-          for (let j = 0; j < changeIcon.length; j++) {
-            changeIcon[j].style.display = "inline";
-          }
-        }
-      )
+    console.log(teamMateListForBtn[0]);
+    // teamMateListForBtn[0].children[0].style.display = "none";
+    teamMateListForBtn[0].children.forEach(function(a) {
+      a.style.display = "none";
+    })
+
+    // teamMateListForBtn[0].children[1].style.display = "none";
+    // teamMateListForBtn[0].children[2].style.display = "none";
+
+  } else {
+      changeSwitch = true;
+      teamMateListForBtn[0].style.display = "";
     }
-  }
-}
-};
+  };
+
+  console.log(typeof(teamMateListForBtn[0].children));
+
+  const entries = Object.entries(teamMateListForBtn);
+
+  console.log(entries);
